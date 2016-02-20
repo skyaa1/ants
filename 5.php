4699016ecd24d813fb5ddd7ce46b8451dfff8316
yang jing pyoung <skyaaaa1@naver.com>
2016-02-20 20:22:01 +0900

    ㄴㅇㅎ

--- 5.php
+++ 5.php
@@ -4,15 +4,9 @@
 		<title></title>
 	</head>
 	<style>
-		
+
 	</style>
 	<body>
-       <?php 
-      echo htmlspecialchars
-      ('<a href="http://http://ec2-54-238-246-145.ap-northeast-1.compute.
-      amazonaws.com/egoing/5.php">');
-       ?>
-       
+      asdfgasdf 
     </body>
 </html>
-
