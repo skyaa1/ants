4e543d732168b164f9ecff138ab77d630a3daab8
yang jing pyoung <skyaaaa1@naver.com>
2016-02-22 12:43:53 +0900

    뷰어 공부

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -4,14 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Topic extends CI_Controller {
 
 
-	public function index()
-	{
-	echo '토픽 펭지1';
+	public function index(){
+	 $this->load->view('topic');
 	}
-  public function get($id1,$as1){
+  public function get($id1){
 
-    echo $id1;
-    echo $as1;
+  $this->load->view('get',array('id'=>$id1));
   }
 
 }
+?>
---
+++ application/views/get.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+  </head>
+  <body>
+     꺼지삼 <?= $id ?>
+
+  </body>
+</html>
