a710a7031b5cf281a447420026c0feab10210323
yang jing pyoung <skyaaaa1@naver.com>
2016-02-23 14:40:04 +0900

    view

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -4,13 +4,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Topic extends CI_Controller {
 
 
-	public function index(){
-	 $this->load->view('topic');
-	}
-  public function get($id1){
+public function index(){
+    $this->load->view('head');
+  $this->load->view('topic');
+    $this->load->view('footer');
+}
+public function main($id1){
+  $this->load->view('head');
 
-  $this->load->view('get',array('id'=>$id1));
-  }
+  $this->load->view('main', array('id'=>$id1));
+  $this->load->view('footer');
+
+}
 
 }
 ?>
--- application/views/topic.php
+++ application/views/topic.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
 
-  </head>
-  <body>
      토픽페이지 입니다
-     
-  </body>
-</html>
+  
